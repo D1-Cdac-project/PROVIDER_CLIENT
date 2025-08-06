555f2ab6f34c32263b1467816b20748be961cbab
Akshay Londhe <akshaylondhe3399@gmail.com>
2025-08-06 18:36:04 +0530

    added booking api's to add the booking, get the booking, and to delete booking

---
+++ src/services/bookingApi.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { getProviderToken } from "../utils/providerCookieUtils";
+
+const BASE_URL = "http://localhost:4000/api/provider";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const providerToken = getProviderToken();
+  if (providerToken) {
+    config.headers.Authorization = `Bearer ${providerToken}`;
+  }
+  if (config.data instanceof FormData) {
+    config.headers["Content-Type"] = "multipart/form-data";
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Booking API Error:", error.response || error.message);
+    if (error.response?.data?.error) {
+      toast.error(error.response.data.error);
+    } else if (error.message) {
+      toast.error("Network error. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
+
+// Booking APIs
+export const getBookingsByProvider = async () => {
+  const response = await api.get("/bookings");
+  return response.data.data.bookings;
+};
+
+export const getBookingById = async (bookingId) => {
+  const response = await api.get(`/booking/get-booking/${bookingId}`);
+  return response.data.data;
+};
